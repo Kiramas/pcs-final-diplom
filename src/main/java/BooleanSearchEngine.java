101409ba9cd73929de8049cf7ce09a530bdd343a
lyraoctavia@gmail.com <lyraoctavia@gmail.com>
2023-06-29 23:47:19 +0500

    init

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,55 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private Map<String, List<PageEntry>> wordsMap;
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        wordsMap = new HashMap<>();
+
+        for (File pdf : pdfsDir.listFiles()) {
+            var doc = new PdfDocument(new PdfReader(pdf));
+            for (int pageNum = 1; pageNum <= doc.getNumberOfPages(); pageNum++) {
+                PdfPage page = doc.getPage(pageNum);
+                String text = PdfTextExtractor.getTextFromPage(page);
+                String[] words = text.split("\\P{IsAlphabetic}+");
+
+                Map<String, Integer> freqs = new HashMap<>();
+
+                for (var word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+
+                for (var entry : freqs.entrySet()) {
+                    List<PageEntry> searchingResult;
+                    if (!wordsMap.containsKey(entry.getKey())) {
+                        searchingResult = new ArrayList<>();
+
+                    } else {
+                        searchingResult = wordsMap.get(entry.getKey());
+                    }
+                    searchingResult.add(new PageEntry(pdf.getName(), pageNum, entry.getValue()));
+                    Collections.sort(searchingResult, Collections.reverseOrder());
+                    wordsMap.put(entry.getKey(), searchingResult);
+                }
+            }
+
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+
+        return wordsMap.get(word.toLowerCase());
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,35 @@
-import java.io.File;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
+    private static final int PORT = 8989;
+
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+        Gson gson = new Gson();
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                ) {
+                    out.println("Введите слова для поиска:");
+                    String message = in.readLine();
+                    List<PageEntry> result = engine.search(message);
+                    out.println(gson.toJson(result));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 }
